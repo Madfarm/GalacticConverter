90d16c10ae3e6c1bdf3155163e75335407906484
AnthonyScott <anthony.scott1005@gmail.com>
2023-10-16 13:35:43 -0400

    Set up the input for weight

--- src/PlanetForm.tsx
+++ src/PlanetForm.tsx
@@ -1,7 +1,16 @@
+import { useState } 
+
 export default function PlanetForm() {
+    const [weight, setWeight] = useState(0);
+
     return (
         <section id="form">
-
+            Weight: 
+            <input
+                value={weight}
+                type="number"
+            >
+            </input>
         </section>
     )
 }
