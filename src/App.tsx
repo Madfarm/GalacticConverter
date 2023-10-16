b85616a22df2df0c0213d7db7b6012127a85422f
AnthonyScott <anthony.scott1005@gmail.com>
2023-10-16 12:50:27 -0400

    Add results section

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,19 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [result, setResult] = useState(null);
+
   return (
     <>
+    <div id="app">
+      <h1>Calculate the weight of an object on a planet</h1>
+      
+      <div id="result">
+        <p>
+          {result}
+        </p>
+      </div>
+    </div>
     </>
   )
 }
